d4f926c886fd3958705d4a4869658c404f054816
Klleriston Andrade <108811295+Klleriston@users.noreply.github.com>
2024-12-16 21:25:24 -0300

    Update install.sh

--- cli-starter-spring-klleriston/install.sh
+++ cli-starter-spring-klleriston/install.sh
@@ -1,32 +1,29 @@
-REPO_URL="https://github.com/klleriston/spring-boilerplate-initializer/releases/download"
-VERSION="v1.0.0" 
-BIN_NAME="spring-cli.jar"
+$RELEASE_URL = "https://github.com/Klleriston/spring-boilerplate-initializer/releases/download/v1.0.0/cli-starter-spring-klleriston-1.0.0-SNAPSHOT.jar"
+$BIN_NAME = "cli-starter-spring-klleriston-1.0.0-SNAPSHOT.jar"
+$INSTALL_DIR = "$HOME\spring-cli"
 
-DOWNLOAD_URL="$REPO_URL/$VERSION/$BIN_NAME"
+Write-Host "Criando o diretório de instalação em $INSTALL_DIR..."
+if (-Not (Test-Path -Path $INSTALL_DIR)) {
+    New-Item -ItemType Directory -Path $INSTALL_DIR | Out-Null
+}
 
-INSTALL_DIR="/usr/local/bin"
+Write-Host "Baixando o arquivo CLI do GitHub..."
+$BIN_PATH = Join-Path -Path $INSTALL_DIR -ChildPath $BIN_NAME
+Invoke-WebRequest -Uri $RELEASE_URL -OutFile $BIN_PATH -UseBasicParsing
 
-if [ "$(id -u)" -ne 0 ]; then
-    echo "Este script precisa ser executado com permissões de administrador (sudo)."
-    echo "Por favor, execute o comando com 'sudo'. Exemplo: sudo bash install.sh"
+if (-Not (Test-Path -Path $BIN_PATH)) {
+    Write-Host "Erro: Não foi possível baixar o arquivo. Verifique sua conexão e o link do GitHub." -ForegroundColor Red
     exit 1
-fi
+}
 
-if ! command -v curl &> /dev/null; then
-    echo "curl não está instalado. Por favor, instale o curl para continuar."
-    exit 1
-fi
+Write-Host "Instalacao concluida com sucesso!" -ForegroundColor Green
+Write-Host "Agora voce pode usar a CLI executando o comando abaixo:"
+Write-Host "java -jar $BIN_PATH" -ForegroundColor Yellow
 
-echo "Baixando o binário da CLI..."
-curl -L -o "$INSTALL_DIR/$BIN_NAME" "$DOWNLOAD_URL"
-
-if [ $? -ne 0 ]; then
-    echo "Erro ao baixar o binário. Verifique se a URL de download está correta."
-    exit 1
-fi
-
-chmod +x "$INSTALL_DIR/$BIN_NAME"
-
-echo "Instalação concluída!"
-echo "Agora você pode usar a CLI executando: spring-cli.jar"
-echo "Para rodá-la, execute: java -jar $INSTALL_DIR/$BIN_NAME"
+Write-Host "----------------------------"
+Write-Host "Agora, apos a execucao do comando 'java -jar $INSTALL_DIR\$BIN_NAME',"
+Write-Host "a CLI ira criar um novo projeto no diretorio onde o comando foi executado."
+Write-Host "Siga as instrucoes no terminal para fornecer o nome do projeto, pacote e outros dados."
+Write-Host "----------------------------"
+Write-Host "Agora voce pode começar a desenvolver seu projeto com o Spring CLI!"
+Write-Host "Boa sorte!" -ForegroundColor Green
